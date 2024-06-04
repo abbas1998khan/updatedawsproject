3e734bc5b95f3afd1a3898da336138d8a363a7b2
abbas1998khan <144901594+abbas1998khan@users.noreply.github.com>
2024-06-04 23:15:18 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -5,7 +5,7 @@ set -e
 sudo docker pull abbas1998khan/sample-python-project
 
 # Run the Docker image as a container
-sudo docker run -d -p 5000:5000 abbas1998khan/samle-python-project
+sudo docker run -d -p 8000:8000 abbas1998khan/samle-python-project
 
 
 
