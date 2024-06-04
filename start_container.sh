582818079a9d6ed7685dbfdd3f4bf5d0d6436e5f
abbas1998khan <144901594+abbas1998khan@users.noreply.github.com>
2024-06-04 16:42:10 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,8 +2,9 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abbas1998khan/sample-python-project
+docker pull abbas1998khan/updatedawsproject
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abbas1998khan/sample-python-project
+docker run -d -p 5000:5000 abbas1998khan/updatedawsproject
+
 
