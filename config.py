d6920d84bbd0e4ea23e3812f767c1765284b0c48
abbas1998khan <144901594+abbas1998khan@users.noreply.github.com>
2024-06-05 22:22:07 +0530

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-customhost = "mats-university-database.cfcks06ged9s.ap-south-1.rds.amazonaws.com"
-customuser = "mats"
-custompass = "matsuniversity"
-customdb = "employee"
-custombucket = "matsemployee"
-customregion = "ap-south-1"
