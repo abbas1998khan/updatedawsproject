bcadc2f6119168e90495904c5ddca7194f8c0b2c
abbas1998khan <144901594+abbas1998khan@users.noreply.github.com>
2024-06-07 02:21:36 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,9 +2,9 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abbas1998khan/sample-html-website
+sudo docker pull abbas1998khan/sample-html-website
 
 
 # Run the Docker image as a container
-docker run -d -p 8080:8080 abbas1998khan/sample-html-website
+sudo docker run -d -p 8080:8080 abbas1998khan/sample-html-website
 
