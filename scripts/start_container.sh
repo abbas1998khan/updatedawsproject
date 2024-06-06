1c98842aab00c8bdbfe67bd5aa544f49340eb3b2
abbas1998khan <144901594+abbas1998khan@users.noreply.github.com>
2024-06-06 06:24:58 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,6 +4,7 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull abbas1998khan/simple-python-flask-app
 
+
 # Run the Docker image as a container
 docker run -d -p 5000:5000 abbas1998khan/simple-python-flask-app
 
